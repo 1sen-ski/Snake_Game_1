0886688114e9b4ef8fa28f42f6d3290d43f3f3ad
Asen Velkov <114012964+1sen-ski@users.noreply.github.com>
2023-02-24 23:33:32 +0200

    Add files via upload

---
+++ snake_game.py
@@ -0,0 +1,149 @@
+import turtle
+import time
+import random
+
+
+def go_up():
+    if snake.direction != "down":
+        snake.direction = "up"
+
+
+def go_down():
+    if snake.direction != "up":
+        snake.direction = "down"
+
+
+def go_left():
+    if snake.direction != "right":
+        snake.direction = "left"
+
+
+def go_right():
+    if snake.direction != "left":
+        snake.direction = "right"
+
+
+def move():
+    if snake.direction == "up":
+        y1 = snake.ycor()
+        snake.sety(y1 + 20)
+    if snake.direction == "down":
+        y1 = snake.ycor()
+        snake.sety(y1 - 20)
+    if snake.direction == "left":
+        x1 = snake.xcor()
+        snake.setx(x1 - 20)
+    if snake.direction == "right":
+        x1 = snake.xcor()
+        snake.setx(x1 + 20)
+
+
+score = 0
+high_score = 0
+delay = 0.1
+
+window1 = turtle.Screen()
+window1.title("Snake Game")
+window1.bgcolor("black")
+window1.setup(width=600, height=600)
+window1.tracer(0)
+
+snake = turtle.Turtle()
+snake.speed(0)
+snake.shape("square")
+snake.color("green")
+snake.penup()
+snake.goto(0, 0)
+snake.direction = "stop"
+
+food = turtle.Turtle()
+food.speed(0)
+food.shape("square")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+scoreboard = turtle.Turtle()
+scoreboard.speed(0)
+scoreboard.shape("square")
+scoreboard.color("yellow")
+scoreboard.penup()
+scoreboard.hideturtle()
+scoreboard.goto(0, 260)
+scoreboard.write("Score: 0   High Score: 0", align="center", font=("ds-digital", 24, "normal"))
+
+window1.listen()
+window1.onkeypress(go_up, "w")
+window1.onkeypress(go_down, "s")
+window1.onkeypress(go_left, "a")
+window1.onkeypress(go_right, "d")
+
+segments = []
+
+while True:
+    window1.update()
+
+    if snake.xcor() > 290 or snake.xcor() < -290 or snake.ycor() > 290 or snake.ycor() < -290:
+        time.sleep(1)
+        snake.goto(0, 0)
+        snake.direction = "stop"
+
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+
+        score = 0
+        delay = 0.1
+
+        scoreboard.clear()
+        scoreboard.write("Score: {}   High Score:   {}".format(score, high_score), align="center",
+                         font=("ds-digital", 24, "normal"))
+    if snake.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("green")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        delay -= 0.001
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        scoreboard.clear()
+        scoreboard.write("Score: {}   High Score:   {}".format(score, high_score), align="center",
+                         font=("ds-digital", 24, "normal"))
+
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+    if len(segments) > 0:
+        x = snake.xcor()
+        y = snake.ycor()
+        segments[0].goto(x, y)
+    move()
+
+    for segment in segments:
+        if segment.distance(snake) < 20:
+            time.sleep(1)
+            snake.goto(0, 0)
+            snake.direction = "stop"
+
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segments.clear()
+            score = 0
+            delay = 0.1
+            scoreboard.clear()
+            scoreboard.write("Score: {}   High Score:   {}".format(score, high_score), align="center",
+                             font=("ds-digital", 24, "normal"))
+    time.sleep(delay)
+
+
+
